564087b229db006f784451109b1d18b468ccb8bf
Vietomato <24521994@gm.uit.edu.vn>
2026-05-20 02:53:23 +0700

    Update spotify-authorize.ts

--- libs/web/auth/data-access/src/lib/models/spotify-authorize.ts
+++ libs/web/auth/data-access/src/lib/models/spotify-authorize.ts
@@ -55,7 +55,7 @@ export class SpotifyAuthorize {
     const params = new URLSearchParams({
       client_id: this.CLIENT_ID,
       response_type: 'code',
-      redirect_uri: 'http://127.0.0.1:4200/',
+      redirect_uri: window.location.origin + '/',
       scope: this.SCOPES.join(' '),
       code_challenge_method: 'S256',
       code_challenge: codeChallenge,
